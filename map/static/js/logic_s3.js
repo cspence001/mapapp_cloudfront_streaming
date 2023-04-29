441f92e4b8c493eb9a72d58e4f3e43165653d535
cspence001 <69818722+cspence001@users.noreply.github.com>
2023-04-28 21:05:56 -0400

    update all select options array, state

--- map/static/js/logic_s3.js
+++ map/static/js/logic_s3.js
@@ -2340,16 +2340,18 @@ var layerControl = L.control.layers.tree (baseMaps, overlaysTree, {
 layerControl.addTo(myMap);
 
 
-// inditems= new Array();
-// filtereditems= new Array();
-//LabelSelectors
 
+//LabelSelectors
 itemsArray= {
+  'STATE':[],
   'IND':[],
-  'LR':[]
+  'LR':[],
+  'JR':[],
+  'BT':[],
+  'BA':[],
+  'LT':[]
 };
-updateVal= new Array();
-filtereditems= new Array();
+
 var htmlObject = layerControl.getContainer().querySelectorAll('input');
 $(htmlObject).on("change", function(e) {
   inditems = new Array();
@@ -2368,12 +2370,22 @@ $(htmlObject).on("change", function(e) {
         } else {
           itemsArray['IND'].push(newobj)
         }//console.log(mainSel, inditem);
-      }); console.log(itemsArray, mainSel, "MultiSel added") //push parameters to websocket 
+      }); console.log(itemsArray,mainSel, "MultiSel added") //push parameters to websocket 
       }
       if (!$(this).parents('.leaflet-layerstree-node:nth(1)')[0]) { //If All Other node checked
         var ancestors = $(this).parents('.leaflet-layerstree-node')[0].children[1].childNodes;
         NodeList.prototype.forEach = Array.prototype.forEach
         ancestors.forEach(item => {
+          if (mainSel == "State") {
+            let stateitem = item.children[0].children[1].innerText;
+            let newstateobj = {stateitem, 'stream':true}
+            var existstateObj = itemsArray['STATE'].find(({stateitem}) => stateitem === newstateobj.stateitem);
+            if(existstateObj) {
+              existstateObj.stream = newstateobj.stream;
+            } else {
+              itemsArray['STATE'].push(newstateobj)
+            }
+          }
           if (mainSel == "Loan Range") {
             let lritem = item.children[0].children[1].innerText;
             let newlrobj = {lritem, 'stream':true}
@@ -2382,6 +2394,46 @@ $(htmlObject).on("change", function(e) {
               existlrObj.stream = newlrobj.stream;
             } else {
               itemsArray['LR'].push(newlrobj)
+            }
+          }
+          if (mainSel == "Job Range") {
+            let jritem = item.children[0].children[1].innerText;
+            let newjrobj = {jritem, 'stream':true}
+            var existjrObj = itemsArray['JR'].find(({jritem}) => jritem === newjrobj.jritem);
+            if(existjrObj) {
+              existjrObj.stream = newjrobj.stream;
+            } else {
+              itemsArray['JR'].push(newjrobj)
+            }
+          }
+          if (mainSel == "Business Type") {
+            let btitem = item.children[0].children[1].innerText;
+            let newbtobj = {btitem, 'stream':true}
+            var existbtObj = itemsArray['BT'].find(({btitem}) => btitem === newbtobj.btitem);
+            if(existbtObj) {
+              existbtObj.stream = newbtobj.stream;
+            } else {
+              itemsArray['BT'].push(newbtobj)
+            }
+          }
+          if (mainSel == "Business Age") {
+            let baitem = item.children[0].children[1].innerText;
+            let newbaobj = {baitem, 'stream':true}
+            var existbaObj = itemsArray['BA'].find(({baitem}) => baitem === newbaobj.baitem);
+            if(existbaObj) {
+              existbaObj.stream = newbaobj.stream;
+            } else {
+              itemsArray['BA'].push(newbaobj)
+            }
+          }
+          if (mainSel == "Loan Type") {
+            let ltitem = item.children[0].children[1].innerText;
+            let newltobj = {ltitem, 'stream':true}
+            var existltObj = itemsArray['LT'].find(({ltitem}) => ltitem === newltobj.ltitem);
+            if(existltObj) {
+              existltObj.stream = newltobj.stream;
+            } else {
+              itemsArray['LT'].push(newltobj)
             }
           }
         });console.log(mainSel, itemsArray, "All Other MultiSel added"); //Loan Range a,b,c,d,e
@@ -2401,12 +2453,21 @@ $(htmlObject).on("change", function(e) {
             }
           });console.log(itemsArray, mainSel, "Ind MultiSel removed")
           //let listContainingRemainingValues = inditems.filter(f => !IND.includes(f))
-          //console.log(mainSel, inditems, "is unchecked"); //Utilities 2211 2212 2213 unchecked
           // ++ push parameters to websocket
         } else { //If All Other unchecked
           var ancestors = $(this).parents('.leaflet-layerstree-node')[0].children[1].childNodes;
           NodeList.prototype.forEach = Array.prototype.forEach
           ancestors.forEach(item =>{
+            if (mainSel == "State") {
+              let stateitem = item.children[0].children[1].innerText;
+              let newstateobj = {stateitem, 'stream':false}
+              var existstateObj = itemsArray['STATE'].find(({stateitem}) => stateitem === newstateobj.stateitem);
+              if(existstateObj) {
+                existstateObj.stream = newstateobj.stream;
+              } else {
+                itemsArray['STATE'].push(newstateobj)
+              }
+            }
             if (mainSel == "Loan Range") {
               let lritem = item.children[0].children[1].innerText;
               let newlrobj = {lritem, 'stream':false}
@@ -2417,7 +2478,47 @@ $(htmlObject).on("change", function(e) {
                 itemsArray['LR'].push(newlrobj)
               }
             }
-          });//console.log(mainSel, item.children[0].children[1].innerText,"is unchecked"); //Loan Rnage a,b,c,d,e unchecked
+            if (mainSel == "Job Range") {
+              let jritem = item.children[0].children[1].innerText;
+              let newjrobj = {jritem, 'stream':false}
+              var existjrObj = itemsArray['JR'].find(({jritem}) => jritem === newjrobj.jritem);
+              if(existjrObj) {
+                existjrObj.stream = newjrobj.stream;
+              } else {
+                itemsArray['JR'].push(newjrobj)
+              }
+            }
+            if (mainSel == "Business Type") {
+              let btitem = item.children[0].children[1].innerText;
+              let newbtobj = {btitem, 'stream':false}
+              var existbtObj = itemsArray['BT'].find(({btitem}) => btitem === newbtobj.btitem);
+              if(existbtObj) {
+                existbtObj.stream = newbtobj.stream;
+              } else {
+                itemsArray['BT'].push(newbtobj)
+              }
+            }
+            if (mainSel == "Business Age") {
+              let baitem = item.children[0].children[1].innerText;
+              let newbaobj = {baitem, 'stream':false}
+              var existbaObj = itemsArray['BA'].find(({baitem}) => baitem === newbaobj.baitem);
+              if(existbaObj) {
+                existbaObj.stream = newbaobj.stream;
+              } else {
+                itemsArray['BA'].push(newbaobj)
+              }
+            }
+            if (mainSel == "Loan Type") {
+              let ltitem = item.children[0].children[1].innerText;
+              let newltobj = {ltitem, 'stream':false}
+              var existltObj = itemsArray['LT'].find(({ltitem}) => ltitem === newltobj.ltitem);
+              if(existltObj) {
+                existltObj.stream = newltobj.stream;
+              } else {
+                itemsArray['LT'].push(newltobj)
+              }
+            }
+          });
           console.log(mainSel, itemsArray, "All Other MultiSel removed")
         }
     } //remove parameters from websocket
@@ -2436,11 +2537,22 @@ $(htmlObject).on("change", function(e) {
           } else {
             itemsArray['IND'].push(newobj)
           }
-          console.log(itemsArray, mainSel, "singleSel added", inditem)
+          console.log(itemsArray,mainSel, "singleSel added", inditem)
         } else { //if Other, single selector checked
           if ($(this).parents('.leaflet-layerstree-node:nth(1)')[0]) {
             if (!$(this).is('.leaflet-control-layers-selector.leaflet-layerstree-sel-all-checkbox')) {
               let mainSel = ($(this).parents('.leaflet-layerstree-node:nth(1)')[0].children[0].children[1].innerText) //main selector Label
+              if (mainSel == "State") {
+                let stateitem = $(this).siblings('span').text()
+                let newstateobj = {stateitem, 'stream':true}
+                var existstateObj = itemsArray['STATE'].find(({stateitem}) => stateitem === newstateobj.stateitem);
+                if(existstateObj) {
+                  existstateObj.stream = newstateobj.stream;
+                } else {
+                  itemsArray['STATE'].push(newstateobj)
+                };
+                console.log(itemsArray, mainSel, "singleSel added", stateitem)
+              }
               if (mainSel == "Loan Range") {
                 let lritem = $(this).siblings('span').text()
                 let newlrobj = {lritem, 'stream':true}
@@ -2451,6 +2563,50 @@ $(htmlObject).on("change", function(e) {
                   itemsArray['LR'].push(newlrobj)
                 };
                 console.log(itemsArray, mainSel, "singleSel added", lritem)
+              }
+              if (mainSel == "Job Range") {
+                let jritem = $(this).siblings('span').text()
+                let newjrobj = {jritem, 'stream':true}
+                var existjrObj = itemsArray['JR'].find(({jritem}) => jritem === newjrobj.jritem);
+                if(existjrObj) {
+                  existjrObj.stream = newjrobj.stream;
+                } else {
+                  itemsArray['JR'].push(newjrobj)
+                };
+                console.log(itemsArray, mainSel, "singleSel added", jritem)
+              }
+              if (mainSel == "Business Type") {
+                let btitem = $(this).siblings('span').text()
+                let newbtobj = {btitem, 'stream':true}
+                var existbtObj = itemsArray['BT'].find(({btitem}) => btitem === newbtobj.btitem);
+                if(existbtObj) {
+                  existbtObj.stream = newbtobj.stream;
+                } else {
+                  itemsArray['BT'].push(newbtobj)
+                };
+                console.log(itemsArray, mainSel, "singleSel added", btitem)
+              }
+              if (mainSel == "Business Age") {
+                let baitem = $(this).siblings('span').text()
+                let newbaobj = {baitem, 'stream':true}
+                var existbaObj = itemsArray['BA'].find(({baitem}) => baitem === newbaobj.baitem);
+                if(existbaObj) {
+                  existbaObj.stream = newbaobj.stream;
+                } else {
+                  itemsArray['BA'].push(newbaobj)
+                };
+                console.log(itemsArray, mainSel, "singleSel added", baitem)
+              }
+              if (mainSel == "Loan Type") {
+                let ltitem = $(this).siblings('span').text()
+                let newltobj = {ltitem, 'stream':true}
+                var existltObj = itemsArray['LT'].find(({ltitem}) => ltitem === newltobj.ltitem);
+                if(existltObj) {
+                  existltObj.stream = newltobj.stream;
+                } else {
+                  itemsArray['LT'].push(newltobj)
+                };
+                console.log(itemsArray, mainSel, "singleSel added", ltitem)
               }
             }
           } 
@@ -2473,6 +2629,17 @@ $(htmlObject).on("change", function(e) {
             console.log(itemsArray, mainSel, "singleSel removed", inditem) //Utilities 2211 unchecked
             } else { //if Other, unchecked
               if (!$(this).is('.leaflet-control-layers-selector.leaflet-layerstree-sel-all-checkbox')) {
+                if (mainSel == "State") {
+                  let stateitem = $(this).siblings('span').text()
+                  let newstateobj = {stateitem, 'stream':false}
+                  var existstateObj = itemsArray['STATE'].find(({stateitem}) => stateitem === newstateobj.stateitem);
+                  if(existstateObj) {
+                    existstateObj.stream = newstateobj.stream;
+                  } else {
+                    itemsArray['STATE'].push(newstateobj)
+                  };
+                  console.log(mainSel, itemsArray, "singleSel removed", stateitem)//All Other SingleSel
+                }
                 if (mainSel == "Loan Range") {
                   let lritem = $(this).siblings('span').text()
                   let newlrobj = {lritem, 'stream':false}
@@ -2483,6 +2650,50 @@ $(htmlObject).on("change", function(e) {
                     itemsArray['LR'].push(newlrobj)
                   };
                   console.log(mainSel, itemsArray, "singleSel removed", lritem)//All Other SingleSel
+                }
+                if (mainSel == "Job Range") {
+                  let jritem = $(this).siblings('span').text()
+                  let newjrobj = {jritem, 'stream':false}
+                  var existjrObj = itemsArray['JR'].find(({jritem}) => jritem === newjrobj.jritem);
+                  if(existjrObj) {
+                    existjrObj.stream = newjrobj.stream;
+                  } else {
+                    itemsArray['JR'].push(newjrobj)
+                  };
+                  console.log(mainSel, itemsArray, "singleSel removed", jritem)//All Other SingleSel
+                }
+                if (mainSel == "Business Type") {
+                  let btitem = $(this).siblings('span').text()
+                  let newbtobj = {btitem, 'stream':false}
+                  var existbtObj = itemsArray['BT'].find(({btitem}) => btitem === newbtobj.btitem);
+                  if(existbtObj) {
+                    existbtObj.stream = newbtobj.stream;
+                  } else {
+                    itemsArray['BT'].push(newbtobj)
+                  };
+                  console.log(itemsArray, mainSel, "singleSel removed", btitem)
+                }
+                if (mainSel == "Business Age") {
+                  let baitem = $(this).siblings('span').text()
+                  let newbaobj = {baitem, 'stream':false}
+                  var existbaObj = itemsArray['BA'].find(({baitem}) => baitem === newbaobj.baitem);
+                  if(existbaObj) {
+                    existbaObj.stream = newbaobj.stream;
+                  } else {
+                    itemsArray['BA'].push(newbaobj)
+                  };
+                  console.log(itemsArray, mainSel, "singleSel removed", baitem)
+                }
+                if (mainSel == "Loan Type") {
+                  let ltitem = $(this).siblings('span').text()
+                  let newltobj = {ltitem, 'stream':false}
+                  var existltObj = itemsArray['LT'].find(({ltitem}) => ltitem === newltobj.ltitem);
+                  if(existltObj) {
+                    existltObj.stream = newltobj.stream;
+                  } else {
+                    itemsArray['LT'].push(newltobj)
+                  };
+                  console.log(itemsArray, mainSel, "singleSel removed", ltitem)
                 }
               }
             }
@@ -3821,18 +4032,6 @@ let objBT = {
   'BT26': [],
 }
 
-// $("#mybutton3").click(function (event) { 
-//   d3.json('/vacountydata', function(data){
-//     var plethstate = L.geoJSON()
-//     plethstate.addData(data)
-//     plethstate.eachLayer(function(layer){
-//         var ptswithin = turf.within(myPts.toGeoJSON(), layer.toGeoJSON());
-//         console.log(ptswithin.features.length +" points in this poly");
-//         console.log(ptswithin);
-//         console.log(plethstate)
-//     })
-//   })
-// })
 
 // CHECKBOX FOR REQUEST ACCESS SINGLE-INSTANCE
 $('input[type="checkbox"]').on('change', function() {
